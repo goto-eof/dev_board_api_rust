c19ab3a2fc7d3be520693f9b4b3efe4d2696379d
Andrei Dodu <andrei.dodu@hotmail.it>
2022-10-26 00:04:45 +0200

    refactor

--- src/daocolumn.rs
+++ src/daocolumn.rs
@@ -1,13 +1,12 @@
 use crate::{
     DBPool, DatabaseConfig, ErrorManager, ErrorManager::Error::*, MapperColumn::*, StructColumns::*,
 };
-
-type Result<T> = std::result::Result<T, ErrorManager::Error>;
+type GenericResult<T> = std::result::Result<T, ErrorManager::Error>;
 
 const TABLE: &str = "db_column_items";
 const SELECT_FIELDS: &str = "ctm_id, ctm_name, created_at";
 
-pub async fn get_by_id(db_pool: &DBPool, id: Option<i32>) -> Result<DbColumnItems> {
+pub async fn get_by_id(db_pool: &DBPool, id: Option<i32>) -> GenericResult<DbColumnItems> {
     let con = DatabaseConfig::get_db_con(db_pool).await?;
 
     let query = format!(
@@ -20,7 +19,7 @@ pub async fn get_by_id(db_pool: &DBPool, id: Option<i32>) -> Result<DbColumnItem
     Ok(row_to_item(&row))
 }
 
-pub async fn fetch_all(db_pool: &DBPool) -> Result<Vec<DbColumnItems>> {
+pub async fn fetch_all(db_pool: &DBPool) -> GenericResult<Vec<DbColumnItems>> {
     let con = DatabaseConfig::get_db_con(db_pool).await?;
 
     let query = format!(
@@ -34,7 +33,7 @@ pub async fn fetch_all(db_pool: &DBPool) -> Result<Vec<DbColumnItems>> {
     Ok(rows.iter().map(|r| row_to_item(&r)).collect())
 }
 
-pub async fn create(db_pool: &DBPool, body: DbColumnItemsRequest) -> Result<DbColumnItems> {
+pub async fn create(db_pool: &DBPool, body: DbColumnItemsRequest) -> GenericResult<DbColumnItems> {
     let con = DatabaseConfig::get_db_con(db_pool).await?;
     let query = format!("INSERT INTO {} (ctm_name) VALUES ($1) RETURNING *", TABLE);
     let row = con
@@ -49,7 +48,7 @@ pub async fn update(
     id: i32,
 
     body: DbColumnItemsUpdateRequest,
-) -> Result<DbColumnItems> {
+) -> GenericResult<DbColumnItems> {
     let con = DatabaseConfig::get_db_con(db_pool).await?;
     let query = format!("UPDATE {} SET ctm_name = $1 WHERE ctm_id = $2", TABLE);
     println!("{}", &query);
@@ -66,7 +65,7 @@ pub async fn update(
     Ok(row_to_item(&row))
 }
 
-pub async fn delete(db_pool: &DBPool, id: i32) -> Result<u64> {
+pub async fn delete(db_pool: &DBPool, id: i32) -> GenericResult<u64> {
     let con = DatabaseConfig::get_db_con(db_pool).await?;
     let query = format!("DELETE FROM {} WHERE ctm_id = $1", TABLE);
     con.execute(query.as_str(), &[&id])
